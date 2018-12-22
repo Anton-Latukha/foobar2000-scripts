598c6e3bc4a1e8b00412d12a6f6b80590f9ac871
Anton-Latukha <anton.latukha@gmail.com>
2018-12-22 03:43:33 +0200

    add get-norm-name.sh

---
+++ get-norm-name.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+files=" \
+macro-originally_released.txt \
+macro-release_d.txt \
+macro-identifier.txt \
+macro-disc_num.txt \
+macro-stream_kHz.txt \
+macro-stream_ch.txt \
+macro-stream_bit.txt \
+macro-stream_specs.txt \
+super-macro-norm-name.txt \
+"
+paste $files | awk '{$2=$2};1'
